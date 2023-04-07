c626873c2cb5dfcf853bf978c3254d9fcbab805b
ColoniG <123653994+ColoniG@users.noreply.github.com>
2023-04-07 20:27:20 -0300

    Initial commit

---
+++ extra/cadastro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RESPOSTA SERVIDOR - PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Os dados foram enviados! \u{1F4BB}"; 
+        ?>
+    </h1>
+    <p>Aprenda PHP para saber o que fazer com eles. 🧑‍💻</p>    
+</body>
+</html>
